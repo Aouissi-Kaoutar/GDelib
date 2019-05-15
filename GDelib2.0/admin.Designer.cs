95fe517b0afad3f5e98b1434fd64c247645eb734
ouissal boubechri <boubechriouissal@gmail.com>
2019-05-15 14:30:17 +0000

    modification

--- GDelib2.0/admin.Designer.cs
+++ GDelib2.0/admin.Designer.cs
@@ -150,7 +150,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 50);
+            this.label1.Location = new System.Drawing.Point(9, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(172, 20);
             this.label1.TabIndex = 3;
@@ -166,6 +166,7 @@
             // 
             // ListeEtd
             // 
+            this.ListeEtd.BackColor = System.Drawing.Color.Black;
             this.ListeEtd.Controls.Add(this.panel2);
             this.ListeEtd.Controls.Add(this.button9);
             this.ListeEtd.Controls.Add(this.button5);
